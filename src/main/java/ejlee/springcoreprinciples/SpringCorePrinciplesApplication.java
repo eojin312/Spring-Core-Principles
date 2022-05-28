fc19a36edb8f162b195f47af73aedbcaf4d856b5
이어진 <ejlee@hanatour.com>
2022-05-28 18:12:21 +0900

    스프링 핵심 원리 - 프로젝트 세팅

---
+++ src/main/java/ejlee/springcoreprinciples/SpringCorePrinciplesApplication.java
@@ -0,0 +1,13 @@
+package ejlee.springcoreprinciples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCorePrinciplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCorePrinciplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ejlee/springcoreprinciples/SpringCorePrinciplesApplicationTests.java
@@ -0,0 +1,13 @@
+package ejlee.springcoreprinciples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCorePrinciplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
