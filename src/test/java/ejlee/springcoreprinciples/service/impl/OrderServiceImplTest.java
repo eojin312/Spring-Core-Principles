742f2f18cc6b681242f697a1e29d3f9b1700c06b
eojin312 <eojin312@naver.com>
2022-07-09 17:48:35 +0900

    orderservice 를 이용해서 회원 정보와 더불어서 상품 주문 테스트

--- src/test/java/ejlee/springcoreprinciples/service/impl/OrderServiceImplTest.java
+++ src/test/java/ejlee/springcoreprinciples/service/impl/OrderServiceImplTest.java
@@ -2,7 +2,9 @@ package ejlee.springcoreprinciples.service.impl;
 
 import ejlee.springcoreprinciples.member.Grade;
 import ejlee.springcoreprinciples.member.Member;
+import ejlee.springcoreprinciples.order.Order;
 import ejlee.springcoreprinciples.service.MemberService;
+import ejlee.springcoreprinciples.service.OrderService;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,6 +14,15 @@ class OrderServiceImplTest {
     @Test
     void createOrder() {
         MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
         Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 1000);
+
+        System.out.println(order);
+
     }
 }
