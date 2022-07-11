e2aef4e92cab2af8667a5cd6dcf0f68a9bb2d974
eojin312 <eojin312@naver.com>
2022-07-11 19:01:43 +0900

    1일1커밋을 지키지 못한 죄

--- src/main/java/ejlee/springcoreprinciples/discount/FixDiscountPolicy.java
+++ src/main/java/ejlee/springcoreprinciples/discount/FixDiscountPolicy.java
@@ -5,7 +5,7 @@ import ejlee.springcoreprinciples.member.Member;
 
 public class FixDiscountPolicy implements DiscountPolicy {
 
-    private int discountFixAmount = 1000;
+    private int discountFixAmount = 10000;
 
     @Override
     public int disCount(Member member, int price) {
